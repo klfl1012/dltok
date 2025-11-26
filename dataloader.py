5d43c48dff460757cac9facaa89c4a5f1124a800
Catacity <wendyhui2371780044@gmail.com>
2025-11-26 17:37:43 +0100

    Dataloader: Adjusted diffusion dataloader to give data without temporal dimension

--- dataloader.py
+++ dataloader.py
@@ -9,6 +9,7 @@ import torch.nn.functional as F
 
 DEFAULT_DATA_ROOTS = {
     'data': Path('/dtu/blackhole/16/223702/data')
+    # 'data': Path('/dtu/blackhole/1b/191611/data')
 }
 
 @dataclass(frozen=True)
@@ -108,15 +109,15 @@ class DiffusionDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
-        x = self.data[idx]  # [T, C, X, Y]
-        y = self.targets[idx]  # [T, C, X, Y]
+        x = self.data[idx]  # [C, X, Y]
+        y = self.targets[idx]  # [C, X, Y]
 
-        # 1. Resize images
+        # 1. Resize images - per channel resizing
         if self.spatial_resolution is not None:
             # 1.1 Merge time and channel dims for interpolation then reshape back
-            T, C, X, Y = x.shape
-            x = x.reshape(T * C, 1, X, Y)
-            y = y.reshape(T * C, 1, X, Y)
+            C, X, Y = x.shape
+            x = x.reshape(C, 1, X, Y)
+            y = y.reshape(C, 1, X, Y)
 
             # 1.2. Resize images
             x = F.interpolate(x, size=(self.spatial_resolution, self.spatial_resolution), 
@@ -125,8 +126,8 @@ class DiffusionDataset(Dataset):
                                 mode='bilinear', align_corners=False)
 
             # 1.3 Reshape back
-            x = x.reshape(T, C, self.spatial_resolution, self.spatial_resolution)
-            y = y.reshape(T, C, self.spatial_resolution, self.spatial_resolution)
+            x = x.reshape(C, self.spatial_resolution, self.spatial_resolution)
+            y = y.reshape(C, self.spatial_resolution, self.spatial_resolution)
 
         # 2 Add Gaussian noise to the resized images
         # 2.1 Get the std for noise
@@ -175,19 +176,19 @@ def build_dataloader(
         std = data_flattened.std(dim=reduce_dims, keepdim=True).clamp_min(1e-6)
         data_flattened = (data_flattened - mean) / std
 
-    X_seq, y_seq = _tensor_to_sequences(data_flattened, seq_len)
-    
-    total_sequences = len(X_seq)
-    train_end = int(total_sequences * train_split)
-    val_end = int(total_sequences * (train_split + val_split))
-    
-    X_train, y_train = X_seq[:train_end], y_seq[:train_end]
-    X_val, y_val = X_seq[train_end:val_end], y_seq[train_end:val_end]
-    X_test, y_test = X_seq[val_end:], y_seq[val_end:]
-
-    splits = [(X_train, y_train), (X_val, y_val), (X_test, y_test)]
-
     if dataset_name == 'PlasmaDataset':
+        X_seq, y_seq = _tensor_to_sequences(data_flattened, seq_len)
+        
+        total_sequences = len(X_seq)
+        train_end = int(total_sequences * train_split)
+        val_end = int(total_sequences * (train_split + val_split))
+        
+        X_train, y_train = X_seq[:train_end], y_seq[:train_end]
+        X_val, y_val = X_seq[train_end:val_end], y_seq[train_end:val_end]
+        X_test, y_test = X_seq[val_end:], y_seq[val_end:]
+
+        splits = [(X_train, y_train), (X_val, y_val), (X_test, y_test)]
+
         train_loader, val_loader, test_loader = [
             DataLoader(
                 PlasmaDataset(X, y, spatial_resolution=spatial_resolution),
@@ -199,6 +200,16 @@ def build_dataloader(
             for X, y in splits
         ]
     else:
+        num_images = data_flattened.shape[0]
+        train_end = int(num_images * train_split)
+        val_end = int(num_images * (train_split + val_split))
+
+        X_train = data_flattened[:train_end]
+        X_val = data_flattened[train_end:val_end]
+        X_test = data_flattened[val_end:]
+
+        splits = [X_train, X_val, X_test]
+
         train_loader, val_loader, test_loader = [
             DataLoader(
                 DiffusionDataset(X, X, spatial_resolution=spatial_resolution, noise_mean = noise_mean ,noise_std = noise_std),
@@ -207,7 +218,7 @@ def build_dataloader(
                 num_workers=num_workers,
                 pin_memory=pin_memory
             )
-            for X, y in splits
+            for X in splits
         ]
      
     print(
